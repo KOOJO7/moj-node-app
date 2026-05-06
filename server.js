8d6d939c06cadf5711fb9d9f7dd7add163b7901e
KOOJO7 <Koszojad2131@gmail.com>
2026-05-06 18:20:45 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(session({
+    secret: 'twoj-bardzo-dlugi-sekretny-klucz', // zmień to na cokolwiek
+    resave: false,
+    saveUninitialized: true
+}));
+
+// TWOJE HASŁO
+const ADMIN_PASSWORD = "TU_WPISZ_SWOJE_HASLO";
+
+// Logowanie
+app.post('/login', (req, res) => {
+    if (req.body.pass === ADMIN_PASSWORD) {
+        req.session.authenticated = true;
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+// Strażnik panelu - jeśli nie jesteś zalogowany, wywali Cię do logowania
+app.get('/panel', (req, res) => {
+    if (req.session.authenticated) {
+        res.sendFile(path.join(__dirname, 'panel.html'));
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => console.log('CYPEK SYSTEM READY ON PORT 3000'));
